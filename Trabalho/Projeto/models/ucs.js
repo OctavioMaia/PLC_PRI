c5fcd31afcb5315d8e9e2b4764ed2f4e0f1321e7
JoaoMVieira <33961226+JoaoMVieira@users.noreply.github.com>
2017-12-14 18:05:34 +0000

    Add: ucs.js
    Adicionei o ucs.js com os 2 schemas principais.

---
+++ Trabalho/Projeto/models/ucs.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var EventoSchema = new Schema({
+    
+
+})
+
+var PublicacaoSchema = new Schema({
+    tipo: {type: String, required: true},
+    privacidade: {type: String, required: true},
+    titulo: {type: String, required: true},
+    data: {type: String, required: true},
+    descricao: {type: String, required: true},
+    local: {type: String, required: false},
+	notas: {"oneOf": []}
+})
+
+var EuSchema = new Schema({
+	ident: {type: String, required: true},
+	nome: {type: String, required: true},
+	idade: {type: String, required: true},
+    genero: {type: String, required: true},
+    email: {type: String, required: true},
+    morada: {type: String, required: true},
+    proficao: {type: String, required: true},
+    numeroTel: {type: String, required: true},
+	publicacoes: [PublicacaoSchema]
+})
+
+//Exportações do módulo
+module.exports= mongoose.model('UC', UCSchema, 'ucs')
