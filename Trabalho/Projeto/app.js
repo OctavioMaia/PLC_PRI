327758da589b72769400fed36eefa7e4cafb3058
Octávio Maia <a71369@alunos.uminho.pt>
2018-01-16 12:16:04 +0000

    Update projeto

--- Trabalho/Projeto/app.js
+++ Trabalho/Projeto/app.js
@@ -1,44 +1,70 @@
+//setup dependancies
 var express = require('express');
 var path = require('path');
-
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var session = require('express-session');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//setup routes
+var routes = require('./routes/index');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//database connection
+var mongoose = require('mongoose')
+var db = mongoose.connect('mongodb://admin:pri2017@ds133077.mlab.com:33077/pri2017-tp', {useMongoClient: true})
+var conn = mongoose.connection;           
+mongoose.Promise = global.Promise;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//cant connect to mLab
+conn.on('error', console.error.bind(console, 'connection error to mLab'));  
+ 
+conn.once('open', function() {
+  console.log("connected to mLab")         
 
-app.use(express.static(path.join(__dirname, 'public')));
+  // use sessions for tracking logins
+  app.use(session({
+    secret: 'pri2017',
+    resave: true,
+    saveUninitialized: false,
+    mongooseConnection: db
+  }));
 
-app.use('/', index);
-app.use('/users', users);
+  // atribute a userID to use in the templates
+  app.use(function (req, res, next) {
+    res.locals.currentUser = req.session.userId;
+    next();
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // parse incoming requests
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
+
+  // routes
+  app.use('/', routes);
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // error handler
+  // define as the last app.use callback
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+
+  // listen on port 3000
+  app.listen(3000, function () {
+    console.log('Listening on port 3000');
+  });
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
---
+++ Trabalho/Projeto/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var UserSchema = new Schema({
+    email: {type: String, required: true},
+    name: {type: String, required: true},
+    password: {type: String, required: true}
+})
+
+module.exports = mongoose.model('User', UserSchema, 'users')
