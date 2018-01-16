b55f16f8fc5d692b67096e425c3fd5597350d377
Octávio Maia <a71369@alunos.uminho.pt>
2018-01-16 17:57:19 +0000

    Added profile editing

--- Trabalho/Projeto/routes/index.js
+++ Trabalho/Projeto/routes/index.js
@@ -3,6 +3,43 @@ var router = express.Router();
 var User = require('../models/user');
 var mid = require('../middleware');
 
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+// GET /register
+router.get('/register', mid.loggedOut, function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// GET /login
+router.get('/login', mid.loggedOut, function(req, res, next) {
+  return res.render('login', { title: 'Login'});
+});
+
+// GET /editprofile
+router.get('/editprofile', mid.requiresLogin, function(req, res, next) {
+  User.findOne({_id: req.session.userId})
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('editprofile', { title: 'Edit your profile', user});
+        }
+      });
+});
+
 // GET /profile
 router.get('/profile', mid.requiresLogin, function(req, res, next) {
   User.findOne({_id: req.session.userId})
@@ -27,11 +64,6 @@ router.get('/logout', function(req, res, next) {
       }
     });
   }
-});
-
-// GET /login
-router.get('/login', mid.loggedOut, function(req, res, next) {
-  return res.render('login', { title: 'Log In'});
 });
 
 // POST /login
@@ -60,14 +92,9 @@ router.post('/login', function(req, res, next) {
   }
 });
 
-// GET /register
-router.get('/register', mid.loggedOut, function(req, res, next) {
-  return res.render('register', { title: 'Sign Up' });
-});
-
 // POST /register
 router.post('/register', function(req, res, next) {
-  if (req.body.email && req.body.name && req.body.password && req.body.confirmPassword) {
+  if (req.body.email && req.body.name && req.body.password && req.body.confirmPassword && req.body.age) {
       // confirm that user typed same password twice
       if (req.body.password !== req.body.confirmPassword) {
         var err = new Error('Passwords do not match.');
@@ -75,10 +102,13 @@ router.post('/register', function(req, res, next) {
         return next(err);
       }
 
+      console.log(req.body)
+
       // create object with form input
       var userData = {
-        email: req.body.email,
         name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
         password: req.body.password
       };
 
@@ -95,12 +125,11 @@ router.post('/register', function(req, res, next) {
             }
           });
         }else{
-          var err = new Error('An account with this username already exists.');
+          var err = new Error('An account with this email already exists.');
           err.status = 400;
           return next(err);
         }
       });
-
     } else {
       var err = new Error('All fields required.');
       err.status = 400;
@@ -108,19 +137,49 @@ router.post('/register', function(req, res, next) {
     }
 })
 
-// GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
-});
+// POST /register
+router.post('/editprofile', function(req, res, next) {
+  if (req.body.name && req.body.password && req.body.confirmPassword) {
+      // confirm that user typed same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        return next(err);
+      }
+      // create object with form input
+      var userData = {
+        name: req.body.name,
+        password: req.body.password,
+        gender: req.body.gender,
+        address: req.body.address,
+        profession: req.body.profession,
+        cnumber: req.body.cnumber
+      };
 
-// GET /about
-router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
-});
+      //finds the current user in order to update
+      User.findOne({_id: req.session.userId}, function(err, user){
+        user.name = userData.name
+        user.password = userData.password
+        if(userData.gender != undefined) user.gender = userData.gender
+        user.address = userData.address
+        user.profession = userData.profession
+        user.cnumber = userData.cnumber
 
-// GET /contact
-router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+        user.save(function(err){
+          if(err){
+            return next(err);
+          }
+          else{
+            req.session.userId = user._id;
+            return res.redirect('/profile');
+          }
+        });
+      });
+    } else {
+      var err = new Error('Name and passwords are mandatory.');
+      err.status = 400;
+      return next(err);
+    }
 });
 
 module.exports = router;
