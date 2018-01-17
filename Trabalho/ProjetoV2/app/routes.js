61fb744ca1d9cc4913d260094c66b827f073bd82
Octávio Maia <a71369@alunos.uminho.pt>
2018-01-17 01:00:51 +0000

    Add projetoV2

---
+++ Trabalho/ProjetoV2/app/routes.js
@@ -0,0 +1,164 @@
+var User       = require('../app/models/user');
+
+module.exports = function(app, passport) {
+// normal routes ===============================================================
+
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index', { title: 'Home'});
+    });
+
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile', { title: 'Profile', user : req.user.local});
+    });
+
+    // LOGOUT ==============================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    // ABOUT ==============================
+    app.get('/about', function(req, res) {
+        res.render('about',{ title: 'About'});
+    });
+
+    // CONTACT ==============================
+    app.get('/contact', function(req, res) {
+        res.render('contact',{ title: 'Contact'});
+    });
+
+    // NEWSFEED ==============================
+    app.get('/newsfeed', function(req, res) {
+        res.render('newsfeed',{ title: 'News Feed'});
+    });
+
+    // EDITPROFILE ==============================
+    app.get('/editprofile',isLoggedIn, function(req, res) {
+        res.render('editprofile',{ title: 'Edit Profile', req});
+    });
+
+    app.post('/editprofile',isLoggedIn , function(req, res, next) {
+        if (req.body.name) {
+            // confirm that user typed same password twice
+            if (req.body.password !== req.body.confirmPassword) {
+              var err = new Error('Passwords do not match.');
+              err.status = 400;
+              return next(err);
+            }
+            // create object with form input
+            var userData = {
+              name: req.body.name,
+              gender: req.body.gender,
+              address: req.body.address,
+              profession: req.body.profession,
+              cnumber: req.body.cnumber
+            };
+      
+            //finds the current user in order to update
+            //console.log(req);
+            User.findOne({ '_id' : req.user.id }, function(err, user){
+              console.log("encontrei: " + user)
+              user.local.name = userData.name
+              user.local.gender = userData.gender
+              user.local.address = userData.address
+              user.local.profession = userData.profession
+              user.local.cnumber = userData.cnumber
+      
+              user.save(function(err){
+                if(err){
+                  return next(err);
+                }
+                else{
+                  return res.redirect('/profile');
+                }
+              });
+            });
+          } else {
+            var err = new Error('Name and passwords are mandatory.');
+            err.status = 400;
+            return next(err);
+          }
+      });
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+    // locally --------------------------------
+        // LOGIN ===============================
+        // show the login form
+        app.get('/login', function(req, res) {
+            res.render('login', { message: req.flash('loginMessage')});
+        });
+
+        // show the locallogin form
+        app.get('/locallogin', function(req, res) {
+            res.render('locallogin', { message: req.flash('loginMessage')});
+        });
+
+        // process the locallogin form
+        app.post('/locallogin', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/error', // redirect back to the login page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // REGISTER =================================
+        // show the register form
+        app.get('/register', function(req, res) {
+            res.render('register', { title: 'Sign Up', message: req.flash('registerMessage')});
+        });
+
+        // process the signup form
+        app.post('/register', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/error', // redirect back to the register page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // LOCALREGISTER ==============================
+        app.get('/localregister', function(req, res) {
+            res.render('localregister',{ title: 'Local Sign Up'});
+        });
+
+        // process the signup form
+        app.post('/localregister', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/error', // redirect back to the register page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+    // facebook -------------------------------
+
+        // send to facebook to do the authentication
+        app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['public_profile', 'email'] }));
+
+        // handle the callback after facebook has authenticated the user
+        app.get('/auth/facebook/callback',
+            passport.authenticate('facebook', {
+                successRedirect : '/profile',
+                failureRedirect : '/'
+            }));
+
+    // google ---------------------------------
+
+        // send to google to do the authentication
+        app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+        // the callback after google has authenticated the user
+        app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                successRedirect : '/profile',
+                failureRedirect : '/'
+            }));
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
---
+++ Trabalho/ProjetoV2/server.js
@@ -0,0 +1,64 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var path     = require('path');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url, {useMongoClient: true}); // connect to our database
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// required for passport
+app.use(session({
+    secret: 'pri2017', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+//error handling
+app.use(function(req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// launch ======================================================================
+app.listen(port);
+console.log('Server listening on port ' + port);
