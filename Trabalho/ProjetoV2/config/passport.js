2667f518a9bf4daed36b3e3ab22d65d9131a5089
Octávio Maia <a71369@alunos.uminho.pt>
2018-01-27 16:48:48 +0000

    Update to final release

--- Trabalho/ProjetoV2/config/passport.js
+++ Trabalho/ProjetoV2/config/passport.js
@@ -89,7 +89,7 @@ module.exports = function(passport) {
 
                     // check to see if theres already a user with that email
                     if (user) {
-                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                        return done(null, false);
                     } else {
                         // create the user
                         var newUser             = new User();
